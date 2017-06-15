8392d9e6fbec2ea5cdd758d6a6a51dbd3b2c883c
stla <laurent_step@yahoo.fr>
2017-06-15 14:37:05 +0200

    update example00

--- inst/testing/example00.R
+++ inst/testing/example00.R
@@ -4,6 +4,7 @@ library(hwriteXLSX)
 
 library(data.table)
 
+set.seed(666)
 dat <- data.table(x   = rnorm(100),
                   y   = rnorm(100),
                   day = Sys.Date() + 1:100)
@@ -17,7 +18,7 @@ for(j in seq_along(dat)){
   for(i in seq_along(column)){
     if(j<3){
       value <- column[[i]]
-      if(abs(value)<3){
+      if(abs(value) < 2){
         D[[c(i+1,j)]] <- cell(j, i+1, value)
       }else{
         D[[c(i+1,j)]] <- cell(j, i+1, value, color="red", bold=TRUE, comment="outlier")
@@ -32,7 +33,7 @@ sheet <- list(Sheet1 = do.call(c, D$values()))
 
 jsonSheet <- jsonlite::toJSON(sheet, null="null", auto_unbox = TRUE)
 
-json2xlsx(jsonSheet, outfile="example00.xlsx")
+#json2xlsx(jsonSheet, outfile="example00.xlsx")
 
 #### add plots ####
 library(ggplot2)
@@ -49,14 +50,21 @@ gg_tsy <-
                date_labels = "%Y-%m-%d") +
   theme(axis.text.x = element_text(angle=45, vjust = 0.5),
         plot.margin = margin(t=0.1, r=1, b=0.1, l=0, "cm"))
-ggsave("scatter.png", gg_scatter, width=6, height=6, units="cm")
-ggsave("ts_x.png", gg_tsx, width=6, height=4, units="cm")
-ggsave("ts_y.png", gg_tsx, width=6, height=4, units="cm")
+# ggsave("scatter.png", gg_scatter, width=6, height=6, units="cm")
+# ggsave("ts_x.png", gg_tsx, width=6, height=4, units="cm")
+# ggsave("ts_y.png", gg_tsx, width=12, height=8, units="cm")
+imageFile1 <- "C:/zTemp/scatter.png"
+imageFile2 <- "C:/zTemp/ts_x.png"
+imageFile3 <- "C:/zTemp/ts_y.png"
+ggsave(imageFile1, gg_scatter, width=12, height=12, units="cm")
+ggsave(imageFile2, gg_tsx, width=12, height=8, units="cm")
+ggsave(imageFile3, gg_tsx, width=12, height=8, units="cm")
 
-image1 <- list(file="scatter.png", left=3, top=1, width=400, height=400)
-image2 <- list(file="ts_x.png", left=3, top=7, width=400, height=267)
-image3 <- list(file="ts_y.png", left=3, top=13, width=400, height=267)
+# plante si fichier sur disque U !
+image1 <- list(file=imageFile1, left=5, top=1, width=400, height=400)
+image2 <- list(file=imageFile2, left=5, top=22, width=400, height=267)
+image3 <- list(file=imageFile3, left=5, top=36, width=400, height=267)
 images <- list(Sheet1 = list(image1, image2, image3))
 jsonImages <- jsonlite::toJSON(images, auto_unbox = TRUE)
 
-json2xlsx(jsonSheet, jsonImages, outfile="example000.xlsx")
+json2xlsx(jsonSheet, jsonImages, outfile="example000.xlsx", overwrite = TRUE)
